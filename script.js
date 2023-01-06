fb50a5f5624aa26e803108605c5d561cffbf8ad3
ShaHuss <117936918+ShaHuss@users.noreply.github.com>
2023-01-06 18:32:33 +0000

    Debugged Characheter selection option - previouosly looping on error

--- script.js
+++ script.js
@@ -2,96 +2,23 @@ let passwordSize = 0;
 // let passwordRepo =[];
 let passwordRepo = [];
 let generatedPassword = "";
-let optionSelected = 0;
+// let optionSelected = 0;
+
+
 // Array of special characters to be included in password
 let specialCharacters = [
-  "@",
-  "%",
-  "+",
-  "\\",
-  "/",
-  "'",
-  "!",
-  "#",
-  "$",
-  "^",
-  "?",
-  ":",
-  ",",
-  ")",
-  "(",
-  "}",
-  "{",
-  "]",
-  "[",
-  "~",
-  "-",
-  "_",
-  ".",
-];
+  "@","%","+","\\","/","'","!","#","$","^","?",":",",",")","(","}","{","]","[","~","-","_",".",];
 
 // Array of numeric characters to be included in password
 let numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 let lowerCasedCharacters = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
+  "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
 
 // Array of uppercase characters to be included in password
 let upperCasedCharacters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
+  "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
 
 // Function to prompt user for password options
 
@@ -107,9 +34,9 @@ function getPasswordOptions() {
   }
 
   // Charachter options
-
+  let optionSelected = 0;
   do {
-    optionSelected = 0;
+    // let optionSelected = 0;
 
     let lowerCaseOption = confirm(
       "Do you want lowercase letters (You must select at least one option)?"
